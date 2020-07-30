16a11a8c46433e18f6b6928cdc45a126b6b5cb6e
Mugdha Shah <shahmugdha15@gmail.com>
2020-07-30 16:57:15 +0530

    express simple router

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 const express = require('express'),
-    http = require('http');
-
+  http = require('http');
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const morgan = require('morgan');
 const port = 3000;
 const bodyParser = require('body-parser');
 const app = express();
-const dishRouter = require('./routes/dishRouter');
+
 app.use(morgan('dev'));
-
-app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-
+app.use('/dishes', dishRouter);
+app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
-
-app.use('/dishes', dishRouter);
-
-
 
 const server = http.createServer(app);
 
